fb0ebaadc3c531b3c7336c4c38a7f65e20d83c2b
erik savage <erik.k.savage@gmail.com>
2022-03-07 22:38:02 -0800

    adds buttons an screen lamp functionality for flashlight app

--- App.js
+++ App.js
@@ -1,12 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button, TouchableOpacity } from 'react-native';
 
 export default function App() {
+  const [lampStatus, setLampStatus] = useState('lampOff');
+
+
+  const toggleLamp = () => {
+    lampStatus === 'lampOff' ? setLampStatus('lampOn') : setLampStatus('lampOff');
+  }
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={[styles.container, styles[lampStatus]]}>
+      <Text style={{ color: '#fff' }}>Lit</Text>
       <StatusBar style="auto" />
+      {/* <Button color='#FFD700' title='Flash Light' /> */}
+      <TouchableOpacity
+        style={styles.button}
+        onPress={toggleLamp}
+      >
+        <Text>LAMP</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -14,8 +29,23 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  lampOn: {
+    backgroundColor: '#ffffff',
+  },
+  lampOff: {
+    backgroundColor: '#222222',
+  },
+  button: {
+    width: 150,
+    height: 25,
+    color: '#000000',
+    backgroundColor: '#FFD700',
+    padding: 20,
+    borderRadius: 10,
+    textAlign: 'center',
+    alignContent: 'center',
+  }
 });
